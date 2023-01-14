2465cc0c461ff5383fa7edbd2060f588c13e998b
Igor Serebryany <igor47@moomers.org>
2023-01-14 06:58:39 -0800

    autocommands from my .vimrc and from source repo

---
+++ user/autocommands.lua
@@ -0,0 +1,66 @@
+-- remove trailing whitespace
+-- autocmd FileType python,php,ruby,javascript,javascript.jsx autocmd BufWritePre <buffer> :%s/\s\+$//e
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*.py", "*.rb", "*.js", "*.ts", "*.tsx", "*.jsx", "*.php" },
+  callback = function()
+		vim.cmd("%s^\\s\\+$^^e")
+  end,
+})
+
+
+-- close lsp pop-ups by pressing 'q'
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+	callback = function()
+		vim.cmd([[
+      nnoremap <silent> <buffer> q :close<CR> 
+      set nobuflisted 
+    ]])
+	end,
+})
+
+-- enable spelling in text files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "gitcommit", "markdown", "email" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+
+-- do something when window is resized? not sure what
+--vim.api.nvim_create_autocmd({ "VimResized" }, {
+--	callback = function()
+--		vim.cmd("tabdo wincmd =")
+--	end,
+--})
+
+-- not sure about this one either
+--vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+--	callback = function()
+--		vim.cmd("quit")
+--	end,
+--})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	callback = function()
+		vim.cmd("hi link illuminatedWord LspReferenceText")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	callback = function()
+	local line_count = vim.api.nvim_buf_line_count(0)
+		if line_count >= 5000 then
+			vim.cmd("IlluminatePauseBuf")
+		end
+	end,
+})
