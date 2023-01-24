1d538cf39e83d8bec9a0fa961dccab85ce6ee791
Igor Serebryany <igor47@moomers.org>
2023-01-23 16:12:53 -0800

    we already have the eslint language server!

--- user/null-ls.lua
+++ user/null-ls.lua
@@ -3,8 +3,5 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint.with({
-      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-    }),
   },
 })
