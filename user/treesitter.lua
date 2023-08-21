702a15227c3fb0a3e4dfecf04b2d47690b7d3dc0
Igor Serebryany <igor47@moomers.org>
2023-08-21 16:06:30 -0700

    fix treesitter config
    only include relevant config

--- user/treesitter.lua
+++ user/treesitter.lua
@@ -13,18 +13,19 @@ configs.setup {
   -- ensure_installed = "all", -- one of "all" or a list of languages
   ignore_install = { "" }, -- List of parsers to ignore installing
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+  auto_install = true,  -- automatically install missing
 
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "css" }, -- list of language that will be disabled
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+    additional_vim_regex_highlighting = false,
   },
-  autopairs = {
-    enable = false,
-  },
-  indent = { enable = true, disable = { } },
 
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+  indent = { enable = true, disable = { } },
 }
