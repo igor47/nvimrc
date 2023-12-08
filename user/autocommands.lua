49022402c5401dcac28aaf43534a76102e89da44
Igor Serebryany <igor47@moomers.org>
2023-12-08 11:41:10 -0800

    disable broken cmd

--- user/autocommands.lua
+++ user/autocommands.lua
@@ -56,11 +56,12 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	callback = function()
-	local line_count = vim.api.nvim_buf_line_count(0)
-		if line_count >= 5000 then
-			vim.cmd("IlluminatePauseBuf")
-		end
-	end,
-})
+-- we don't use https://github.com/RRethy/vim-illuminate
+-- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+-- 	callback = function()
+-- 	local line_count = vim.api.nvim_buf_line_count(0)
+-- 		if line_count >= 5000 then
+-- 			vim.cmd("IlluminatePauseBuf")
+-- 		end
+-- 	end,
+-- })
