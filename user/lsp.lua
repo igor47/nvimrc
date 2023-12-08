b8a1cdc61b978003788dd57fe35e3d7e4e878c63
Igor Serebryany <igor47@moomers.org>
2023-12-08 11:41:24 -0800

    switch to pyright
    pylsp doesn't seem to work very well

--- user/lsp.lua
+++ user/lsp.lua
@@ -71,33 +71,4 @@ require("mason-lspconfig").setup_handlers({
       }
     })
   end,
-  ["pylsp"] = function ()
-    -- based on https://jdhao.github.io/2023/07/22/neovim-pylsp-setup/
-    lspconfig.pylsp.setup({
-      capabilities = capabilities,
-      flags = {
-        debounce_text_changes = 200,
-      },
-      settings = {
-        pylsp = {
-          plugins = {
-            -- formatter options
-            black = { enabled = true },
-            autopep8 = { enabled = false },
-            yapf = { enabled = false },
-            -- linter options
-            pylint = { enabled = true, executable = "pylint" },
-            pyflakes = { enabled = false },
-            pycodestyle = { enabled = false },
-            -- type checker
-            pylsp_mypy = { enabled = true },
-            -- auto-completion options
-            jedi_completion = { fuzzy = true },
-            -- import sorting
-            pyls_isort = { enabled = true },
-          },
-        }
-      },
-    })
-  end,
 })
